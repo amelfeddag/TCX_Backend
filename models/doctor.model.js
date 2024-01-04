54bbca09e43032040739d8608e212d94768b673d
amelfeddag <amel1feddag@gmail.com>
2024-01-05 00:04:32 +0100

    something

---
+++ models/doctor.model.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    
+}, {
+    timestamps: true
+})
+
+export const UserModel = mongoose.model('users', userSchema);
