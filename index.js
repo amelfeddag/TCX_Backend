3dbc2f07c1312b2120256cc237779f699095ca84
amelfeddag <amel1feddag@gmail.com>
2024-01-06 09:22:58 +0100

    first one

--- index.js
+++ index.js
@@ -1,29 +1,61 @@
-import mongoose from 'mongoose'
-import { UserModel } from './models/doctor.model.js';
-mongoose
-  .connect("mongodb://localhost/dev")
-  .then(async () => {
-    // once connected, give a success message
-    console.log("Connected to MongoDB");
-    // Make operations on user model
+import express from 'express';
+import db from './config/db.js';
+import signinRouter from './api/sign_up_router.route.js';
+import loginRouter from './api/log_in_.router.js';
+import verifyRouter from './api/verify_email_router.js';
+import  historyController from './api/history_router.js';
+import logDoctorAction  from './api/mdlw_history.js'; 
+import patientController from './api/doc_router.js';
+import rdvController from './api/rdv_router.js';
+import resultcontroller from './api/results_router.js';
+import imgController from './api/img_router.js';
+import dmController from './api/dm_router.js';
+import prescriController from './api/prescri_router.js';
+import forgot from './api/forgot_router.js';
 
-    // Delete
-    const r = await UserModel.deleteOne({
-        name: 'Test 2'
-    })
-    // // Delete many
-    // await UserModel.deleteMany({
-    //     name: 'Test'
-    // })
-    // // Find and delete
-    // await UserModel.findOneAndDelete({
-    //     name: 'Test'
-    // })
-    console.log(r)
-    await mongoose.disconnect()
+const app = express();
+const PORT = 3004;
+
+app.use(express.json());
+
+// Connect to the database
+db()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
   })
-  .catch(async err => {
-    // if something goes wrong let us know
-    console.log(err);
-    await mongoose.disconnect()
+  .catch((error) => {
+    console.error('Error:', error);
   });
+
+
+app.use('/doctor', signinRouter);
+
+app.use('/doctor', loginRouter);
+
+app.use('/doctor', verifyRouter);
+
+app.use('/doctors',patientController)
+
+
+app.use('/doctors', logDoctorAction);
+app.use('/doctors', historyController);
+app.use('/doctors/:doctorId/appointments',rdvController);
+
+
+//results
+app.use('/doctors/:doctorId/patients/:patientId/medical-records/:recordId/results', resultcontroller);
+
+//img
+app.use('/doctors/:doctorId/patients/:patientId/medical-records/:recordId/images', imgController);
+
+
+//dm
+
+app.use('/doctors/:doctorId/patients/:patientId/medical-records', dmController);
+//prsecri
+
+app.use('/doctors/:doctorId/patients/:patientId/medical-records/:recordId/prescriptions', prescriController);
+
+app.use('/users/forgot-password',forgot);
